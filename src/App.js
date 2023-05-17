b4aaa825a3bfc15ddba484198529936d47740567
Ananth Narayan <narayanananth26@gmail.com>
2023-05-17 17:30:12 +0530

    implement form to input new expense

--- src/App.js
+++ src/App.js
@@ -25,9 +25,14 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = expense => {
+    console.log('in App.js', expense);  
+  }
+
   return (
     <div>
-      <NewExpense/>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses expenses={expenses} />
     </div>
   );
