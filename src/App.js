ff4ddfc9a033a044d55af0e7d21abf85c8cc58bf
Ananth Narayan <narayanananth26@gmail.com>
2023-05-17 11:41:25 +0530

    organize comp files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import ExpenseItem from './components/ExpenseItem';
-import Expenses from './components/Expenses';
+import ExpenseItem from './components/Expenses/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
   const expenses = [
@@ -26,7 +26,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Expenses expenses={expenses}/>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
